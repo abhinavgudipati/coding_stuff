b9c0363f7e87896f5c1d02e3d2bdd7ec19fb8114
abhinavgudipati <53547094+abhinavgudipati@users.noreply.github.com>
2022-04-18 12:26:29 +0530

    Add files via upload

---
+++ firstProblem.java
@@ -0,0 +1,25 @@
+public class firstProblem{
+
+    static void reverseArray(int arr[] , int start, int end){
+        int temp ; 
+        while(start < end ){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp; 
+            start++;
+            end--;
+        }
+    }
+    static void printArray(int arr[], int size){
+        for( int i = 0 ; i < size ; i++ ){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6,7,8};
+        int arrLength = arr.length; 
+        // printArray(arr, arrLength);
+        reverseArray(arr, 0 ,arrLength-1);
+        printArray(arr,arrLength);
+    }
+}
