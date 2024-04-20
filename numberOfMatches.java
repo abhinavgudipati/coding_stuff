4766dd2c0635b86d4c3f8b0fe4abb7df1d541604
1192704 <gabhinavsiddharth@gmail.com>
2024-04-21 02:36:41 +0530

    Number of Matches - 21.04.2024

---
+++ numberOfMatches.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int count = 0;
+        while(n>=2){
+            if(n%2==1){
+                count = count + (n-1)/2;
+                n = (n-1)/2 + 1;
+            }
+            if(n%2==0){
+                count = count + n/2;
+                n = n/2;
+            }
+        }
+        return count;
+    }
+}
